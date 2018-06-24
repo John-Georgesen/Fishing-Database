a2c72bbc053f1512114cf7f83ba6972fb00f796c
John Georgesen <jwgeorgesen@gmail.com>
2018-06-23 23:23:13 -0400

    First Commit.
    This program is a MySQL manipulative program that was coded in Java.
    It allows the user to easily manipulate their database without learning
    the complex MySQL commands.

--- FishingDB/src/execute.java
+++ FishingDB/src/execute.java
@@ -1,4 +1,3 @@
-
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -6,14 +5,51 @@ public class execute
 {
 	public static boolean done = false;
 	
+	//the user interacting object that all the classes use
+	public static UserInteraction inter = new UserInteraction();
+	
 	public static void main(String[] args) throws SQLException
 	{
-		//create login screen
-		loginGUI loginScreen = new loginGUI();
+		boolean done = false;
 		
-		loginScreen.createGUI();
+		String response = null;
 		
-		loginScreen.mainFrame.repaint();
+		//login the user
+		inter.login();
 		
+		//connect to the database
+		Connect con = new Connect();
+		System.out.println("Successfully Connected!");
+
+		Scanner scan = new Scanner(System.in);
+		
+		//create a looping command line
+		do
+		{
+			System.out.print("Would you like to continue (yes of no)?: ");
+			response = scan.nextLine();
+			
+			response = response.toLowerCase();
+			
+			if(response.equals("yes"))
+			{
+				//ask user (edit,add,remove, or get)
+				inter.askUser();
+				
+			}
+			//if user declines offer to continue
+			else if(response.equals("no"))
+			{
+				System.out.println("Exiting...");
+				
+				//break from the loop
+				done = true;
+			}
+			else
+			{
+				System.out.println("Enter a valid command");
+			}
+		}
+		while(done != true);
 	}
 }
